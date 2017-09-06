476f1705d3866b3776f929279b12a1589865c612
BrandonMartel <martelbrandon99@gmail.com>
2017-09-06 11:14:41 -0500

    morning gists

---
+++ Morning_Gists/IntrotoCpp/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+	int a = 1;
+	int b = 2;
+	int c = 3;
+	int d = 4;
+	int e = 5;
+	int bigNum = 0;
+	int otherBigNum = 0;
+
+	int array[] = { a, b, c, d, e };
+	
+	for (int i = 0; i <= 5; i++)
+	{
+		
+	}
+
+	bigNum = d;
+	otherBigNum = e;
+	std::cout << "The sum of the two greatest values is " << d + e << std::endl;
+	system("pause");
+}
